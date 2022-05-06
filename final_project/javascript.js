83a1a82d6f9eab23a14954588176f507a23117a3
cjdorsi <cjdorsi@syr.edu>
2022-05-06 16:05:08 -0400

    blur covers

---
+++ final_project/javascript.js
@@ -0,0 +1,12 @@
+function changePpl () {
+    document.getElementById("pplwekeepblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/the_people_we_keep_blur.jpg');
+}
+function changePplBack () {
+    document.getElementById("pplwekeepblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/the_people_we_keep.jpg');
+}
+function changeCrawdads () {
+    document.getElementById("crawdadsblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/where_the_crawdads_sing_blur.jpg');
+}
+function changeCrawdadsBack () {
+    document.getElementById("crawdadsblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/where_the_crawdads_sing.jpg');
+}
