d090e235903a09a2672177dfbf92614f0bbe9161
cjdorsi <cjdorsi@syr.edu>
2022-05-06 16:36:28 -0400

    blur book covers

--- final_project/javascript.js
+++ final_project/javascript.js
@@ -4,9 +4,52 @@ function changePpl () {
 function changePplBack () {
     document.getElementById("pplwekeepblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/the_people_we_keep.jpg');
 }
+
 function changeCrawdads () {
     document.getElementById("crawdadsblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/where_the_crawdads_sing_blur.jpg');
 }
 function changeCrawdadsBack () {
     document.getElementById("crawdadsblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/where_the_crawdads_sing.jpg');
+}
+
+function changeGreatAlone () {
+    document.getElementById("greataloneblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/the_great_alone_blur.jpg');
+}
+function changeGreatAloneBack () {
+    document.getElementById("greataloneblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/the_great_alone.jpg');
+}
+
+function changeTheHelp () {
+    document.getElementById("thehelpblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/the_help_blur.jpg');
+}
+function changeTheHelpBack () {
+    document.getElementById("thehelpblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/the_help.jpg');
+}
+
+function changeLittleLife () {
+    document.getElementById("alittlelifeblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/a_little_life_blur.jpg');
+}
+function changeLittleLifeBack () {
+    document.getElementById("alittlelifeblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/a_little_life.jpg');
+}
+
+function changeHMart () {
+    document.getElementById("hmartblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/crying_in_h_mart_blur.jpg');
+}
+function changeHMartBack () {
+    document.getElementById("hmartblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/crying_in_h_mart.jpg');
+}
+
+function changeAlchemist () {
+    document.getElementById("thealchemistblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/the_alchemist_blur.jpg');
+}
+function changeAlchemistBack () {
+    document.getElementById("thealchemistblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/the_alchemist.jpg');
+}
+
+function changeBeautifulWorld () {
+    document.getElementById("beautifulworldblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/beautiful_world_where_are_you_blur.jpg');
+}
+function changeBeautifulWorldBack () {
+    document.getElementById("beautifulworldblur").setAttribute('src', 'https://cjdorsi.github.io/ist263/project/media/book_covers/beautiful_world_where_are_you.jpg');
 }
